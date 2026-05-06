b2cf4fe9d2130cc7110d9526a5f45fd29d60bccf
Mahayoga <myoga.bahtiar@gmail.com>
2026-05-07 04:44:09 +0700

    WIP: data buat heatmap

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 import numpy as np
+from seaborn import heatmap
+import random
 
 app = FastAPI()
 
@@ -39,6 +41,37 @@ async def extract_features(request: Request):
     df['dt'] = df['timestamp'].diff() / 1000
 
     df = df.fillna(0)
+
+    # =============================
+    # 🔥 BUBBLE GRID
+    # =============================
+    # GRID_SIZE = 30  # makin besar makin detail
+    # df['x_norm'] = df['gaze_x'] / SCREEN_W
+    # df['y_norm'] = df['gaze_y'] / SCREEN_H
+
+    # # mapping ke grid
+    # df['x_bin'] = (df['x_norm'] * GRID_SIZE).astype(int)
+    # df['y_bin'] = (df['y_norm'] * GRID_SIZE).astype(int)
+
+    # # hitung frekuensi
+    # bubble_grid = df.groupby(['x_bin', 'y_bin']).agg({
+    #     'gaze_x': 'mean',
+    #     'gaze_y': 'mean',
+    #     'timestamp': 'count'
+    # }).reset_index()
+    # bubble_grid.rename(columns={'timestamp': 'count'}, inplace=True)
+
+    # # cari max buat scaling radius
+    # max_count = bubble_grid['count'].max()
+
+    # convert ke format heatmap
+    heatmap_data = []
+    for _, row in df.iterrows():
+        heatmap_data.append({
+            "x": int(row['gaze_x']),
+            "y": int(row['gaze_y']),
+            "value": 1
+        })
 
     # =============================
     # ⚡ VELOCITY & DISTANCE
@@ -104,6 +137,7 @@ async def extract_features(request: Request):
     return {
         "global_features": features,
         "window_features": windows,
+        "heatmap": heatmap_data,
         "timeseries": {
             "timestamp": df_down['timestamp'].tolist(),
             "velocity": df_down['velocity'].tolist()
